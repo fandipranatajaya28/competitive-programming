25b7097346398ba50147f6156d1e1a18d852eac4
Fandi Pranata Jaya <fandipj98@gmail.com>
2025-12-18 23:19:27 +0700

    feat: Add LeetCode concatenation-of-array solution

---
+++ LeetCode/concatenation-of-array/concatenation-of-array.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func getConcatenation(nums []int) []int {
+	n := len(nums)
+	ans := make([]int, 2*n)
+	for i := 0; i < n; i++ {
+		ans[i] = nums[i]
+		ans[i+n] = nums[i]
+	}
+	return ans
+}
+
+func main() {
+	fmt.Println(getConcatenation([]int{1, 2, 1}))
+}
