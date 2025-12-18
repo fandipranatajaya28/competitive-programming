95acaf38fdf42015144e51f0e23c5bbfe51d6774
Fandi Pranata Jaya <fandipj98@gmail.com>
2025-12-18 23:32:52 +0700

    feat: Update LeetCode two-sum solution

--- LeetCode/two-sum/two-sum.go
+++ LeetCode/two-sum/two-sum.go
@@ -2,53 +2,20 @@ package main
 
 import (
 	"fmt"
-	"sort"
 )
 
-func twoSum(nums []int, target int) (result []int) {
-	// class to store value and its original index
-	type number struct {
-		value int
-		index int
-	}
-
-	var (
-		left, right int
-		numsArr     []number
-	)
-
-	for i, v := range nums {
-		numsArr = append(numsArr, number{
-			value: v,
-			index: i,
-		})
-	}
-
-	// sort the elements
-	sort.Slice(numsArr, func(i, j int) bool {
-		return numsArr[i].value < numsArr[j].value
-	})
-
-	// init pointers
-	left = 0
-	right = len(numsArr) - 1
-
-	for left < right {
-		if numsArr[left].value+numsArr[right].value == target {
-			result = append(result, numsArr[left].index, numsArr[right].index)
-			return
-		} else if numsArr[left].value+numsArr[right].value < target {
-			left++
-		} else { // numsArr[left].value+numsArr[right].value > target
-			right--
+func twoSum(nums []int, target int) []int {
+	var numMap = make(map[int]int)
+	for idx, num := range nums {
+		remainder := target - num
+		if remainderIdx, isExist := numMap[remainder]; isExist {
+			return []int{idx, remainderIdx}
 		}
+		numMap[num] = idx
 	}
-	return
+	return nil
 }
 
 func main() {
-	nums := []int{11, 15, 2, 7}
-	target := 9
-	result := twoSum(nums, target)
-	fmt.Println(result)
+	fmt.Println(twoSum([]int{2, 7, 11, 15}, 9))
 }
