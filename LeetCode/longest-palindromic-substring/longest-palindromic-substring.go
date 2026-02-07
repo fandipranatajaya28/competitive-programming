33c7ce0cfe4a974803d28ec33ab8f05b13386aa8
Fandi Pranata Jaya <fandipj98@gmail.com>
2026-02-07 23:31:20 +0700

    feat: Modify LeetCode longest-palindromic-substring solution

--- LeetCode/longest-palindromic-substring/longest-palindromic-substring.go
+++ LeetCode/longest-palindromic-substring/longest-palindromic-substring.go
@@ -5,8 +5,33 @@ import (
 	"strings"
 )
 
-// Using Manacher's algorithm https://www.youtube.com/watch?v=6Bq8j2dhzJc
 func longestPalindrome(s string) string {
+	var result string
+	for i := 0; i < len(s); i++ {
+		oddPalindrome := expandFromCenter(s, i, i)
+		evenPalindrome := expandFromCenter(s, i, i+1)
+
+		if len(oddPalindrome) > len(result) {
+			result = oddPalindrome
+		}
+
+		if len(evenPalindrome) > len(result) {
+			result = evenPalindrome
+		}
+	}
+	return result
+}
+
+func expandFromCenter(s string, l int, r int) string {
+	for l >= 0 && r < len(s) && s[l] == s[r] {
+		l--
+		r++
+	}
+	return s[l+1 : r]
+}
+
+// Using Manacher's algorithm https://www.youtube.com/watch?v=6Bq8j2dhzJc
+func longestPalindromeManacher(s string) string {
 	T := "^#" + strings.Join(strings.Split(s, ""), "#") + "#$"
 	n := len(T)
 	P := make([]int, n)
