c22e0ce65398cd6d99f9cf3ed06d8ba87edf235e
Fandi Pranata Jaya <fandipj98@gmail.com>
2026-02-09 00:53:07 +0700

    feat: Modify LeetCode find-the-length-of-the-longest-common-prefix solution

--- LeetCode/find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.go
+++ LeetCode/find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.go
@@ -3,42 +3,44 @@ package main
 import "fmt"
 
 func longestCommonPrefix(arr1 []int, arr2 []int) int {
-	var (
-		lcpLen      int
-		prefixesMap = make(map[int]bool)
-	)
+	prefixMap := make(map[int]int) // map[prefix]length
 
+	// Store all prefixes with their lengths
 	for _, num := range arr1 {
-		for num != 0 {
-			prefixesMap[num] = true
+		length := 0
+		temp := num
+		for temp > 0 {
+			length++
+			temp /= 10
+		}
+
+		// Generate all prefixes
+		for num > 0 {
+			prefixMap[num] = length
 			num /= 10
+			length--
 		}
 	}
 
+	maxLen := 0
+
+	// Find longest matching prefix
 	for _, num := range arr2 {
-		for num != 0 {
-			if prefixesMap[num] {
-				len := intLen(num)
-				if len > lcpLen {
-					lcpLen = len
+		for num > 0 {
+			if length, exists := prefixMap[num]; exists {
+				if length > maxLen {
+					maxLen = length
 				}
+				break // Found longest for this number
 			}
 			num /= 10
 		}
 	}
 
-	return lcpLen
-}
-
-func intLen(num int) int {
-	var len int
-	for num != 0 {
-		len++
-		num /= 10
-	}
-	return len
+	return maxLen
 }
 
 func main() {
-	fmt.Println(longestCommonPrefix([]int{1, 10, 100}, []int{1000}))
+	fmt.Println(longestCommonPrefix([]int{1, 10, 100}, []int{1000})) // 3
+	fmt.Println(longestCommonPrefix([]int{1, 2, 3}, []int{4, 4, 4})) // 0
 }
